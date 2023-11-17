8fac80f4e1e342344acf4c6b0b7bad154a69bc2c
fefestuve <123496126+fefestuve@users.noreply.github.com>
2023-11-17 09:53:59 -0300

    .

---
+++ C/ejemplos-practicas/numeros_primos.c
@@ -0,0 +1,48 @@
+//Este codigo imprime todos los números primos entre un número mínimo y uno máximo
+#include <stdio.h>
+
+int prime(int number);
+
+int main(void)
+{
+    int min;
+    do
+    {
+        printf("Minimo: ");
+        scanf("%d", &min);
+    }
+    while (min < 1);
+
+    int max;
+    do
+    {
+        printf("Maximo: ");
+        scanf("%d", &max);
+    }
+    while (min >= max);
+
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
+        }
+    }
+}
+
+int prime(int i)
+{
+    int pepe =1;
+    if(i==1){
+        pepe=0;
+        return pepe;
+    }
+    for (int j = 2; j < i; j++){
+
+        if (i % j == 0){
+            pepe = 0;
+            return pepe;
+        }
+    }
+    return pepe;
+}
