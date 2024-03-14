d6954e69dba194913e231637447a8ddb3be247df
fefestuve <123496126+fefestuve@users.noreply.github.com>
2024-03-14 12:39:47 -0300

    Update UnionFloats.c

--- LT6/UnionFloats.c
+++ LT6/UnionFloats.c
@@ -3,15 +3,20 @@
 union numeros{
     float var;
     unsigned char vect[4];
+    struct{
+        unsigned int signo : 1;
+        unsigned int exponente : 8;
+        unsigned int mantisa : 23;
+    }bits;
 };
 
 int main(void){
     union numeros numero;
     printf("Ingresa un numero \n");
     scanf("%f",&numero.var);
-    printf("El numero que ingresaste esta guardado en la memoria como ");
+    printf("El numero que ingresaste esta guardado en la memoria como 0x");
     for (int i = 3; i >= 0; i--){
-        printf("%x",numero.vect[i]);
+        printf("%x|",numero.vect[i]);
     }
     printf(" en hexadecimal\n");
 }
